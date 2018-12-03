3fe77c758aa401c9137a77fe3cfebfe47de47029
Wes Rast <wes.rast@gmail.com>
2018-12-02 23:31:27 -0500

    Add project explanation.

--- src/Schedule/index.js
+++ src/Schedule/index.js
@@ -6,9 +6,15 @@ import ContextExample from './ContextExample';
 const ScheduleStyled = styled.section`
 	display: grid;
 	grid-template-columns: repeat(2, 50%);
-	grid-template-rows: 60px 1fr;
+	grid-template-rows: 80px 60px 1fr;
 	margin: 0 10%;
 	min-height: 85vh;
+`;
+
+const AsideStyled = styled.aside`
+	text-align: left;
+	padding: 10px 0;
+	grid-column: 1/3;
 `;
 
 const LabelStyled = styled.h3`
@@ -21,6 +27,14 @@ const SectionStyled = styled.section`
 
 const Schedule = () => (
 	<ScheduleStyled>
+		<AsideStyled>
+			<p>
+				While the instructions called only for a Redux solution, I thought the
+				problem lent itself to a Context based state, so I've added a second
+				section here to show how that would be done. Both solutions use the same
+				shared UI logic and rendering Components.
+			</p>
+		</AsideStyled>
 		<LabelStyled>Redux State</LabelStyled>
 		<LabelStyled>Context State</LabelStyled>
 		<SectionStyled>
